0eb59abb96afec19e10f56d0910a2e5acd58e440
Priyanshu kumar <priyanshukumarsingh9484@gmail.com>
2025-05-16 23:38:38 +0530

    Commit

--- skilllink-frontend/src/pages/Customer/CustomerHome.jsx
+++ skilllink-frontend/src/pages/Customer/CustomerHome.jsx
@@ -31,7 +31,7 @@ const services = [
 ];
 
 const CustomerHome = () => {
-  const naviaget = useNavigate();
+  const navigaet = useNavigate();
   return (
     <>
       <Navbar />
@@ -51,7 +51,7 @@ const CustomerHome = () => {
                 <h2 className="text-xl font-semibold text-gray-800">{service.title}</h2>
                 <p className="text-gray-600 mt-2">{service.description}</p>
                 <button className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-                onClick={()=>naviaget(`/service/${service.title.toLowerCase()}`)}>
+                onClick={()=>navigate(`/service/${service.title.toLowerCase()}`)}>
                   View {service.title}s
                 </button>
               </div>
