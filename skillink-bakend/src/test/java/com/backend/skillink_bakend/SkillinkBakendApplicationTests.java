4efb1242fa709e6163ce26344d3666481b3174ca
Priyanshu kumar <priyanshukumarsingh9484@gmail.com>
2025-05-23 23:17:37 +0530

    Commit

---
+++ skillink-bakend/src/test/java/com/backend/skillink_bakend/SkillinkBakendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.skillink_bakend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillinkBakendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
